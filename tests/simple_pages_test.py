c18012fe26dd3fbd2b3e79449ff0afdf79182b3c
MohitJoshi <70653516+mj434@users.noreply.github.com>
2022-03-07 23:22:00 -0500

    dev bootstrap

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -6,7 +6,7 @@ def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b'<li><a href="/page/about">About</a></li>' in response.data
+    assert b'About' in response.data
 
 
 
